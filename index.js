be728457bd0d4852ab3e397821287b9a3acf52d8
Mario-Chavez <marioachavez1@gmail.com>
2023-06-21 10:45:36 -0300

    agregamos un puerto con express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import express from "express";
 
-console.log("estoy");
+// configurar  un puerto
+// crear una instancia de express
+const app = express();
+
+// crear puerto
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+    console.log("corriendo en port:", app.get("port"));
+});
