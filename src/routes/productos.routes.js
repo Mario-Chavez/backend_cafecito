150439f7f062448b8e09000c45c01ce254edfc9f
Mario-Chavez <marioachavez1@gmail.com>
2023-07-14 16:20:18 -0300

    se uso jwt para usar el token del ususario

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,17 +7,21 @@ import {
     obtenerUnProductos,
 } from "../controllers/productos.controllers";
 import validarProducto from "../helpers/validarProducto";
+import validarJWT from "../helpers/token-verify";
 
 const router = Router();
 
 // Rutas
-router.route("/productos").get(obtenerProductos).post(validarProducto, crearProducto);
+router
+    .route("/productos")
+    .get(obtenerProductos)
+    .post([validarJWT, validarProducto], crearProducto);
 
 // delete, put and find one product
 router
     .route("/productos/:id")
     .delete(borrarProducto)
-    .put(validarProducto, editarProducto)
+    .put([validarJWT, validarProducto], editarProducto)
     .get(obtenerUnProductos);
 
 export default router;
