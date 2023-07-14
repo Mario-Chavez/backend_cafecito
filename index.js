7bb0c6299ef76d1ddb51043a2e23d1d918f72a49
Mario-Chavez <marioachavez1@gmail.com>
2023-07-14 11:24:43 -0300

    se agrego un archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+import path from "path";
 import "./src/database/dbConnections";
 import productosRouter from "./src/routes/productos.routes";
 import userRouter from "./src/routes/user.routes";
@@ -22,6 +23,7 @@ app.use(cors()); // permite conecciones remotas
 app.use(express.json()); //permite interpretar el formato json
 app.use(express.urlencoded({ extended: true })); //permite en el objeto requiest string o array
 app.use(morgan("dev")); // nos da informacion extra en la terminal
+app.use(express.static(path.join(__dirname, "/public"))); //para darle una ruta estatica
 
 app.use("/apicafe", productosRouter);
 app.use("/apicafe/auth", userRouter);
